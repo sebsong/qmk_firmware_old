fbd91c145dd22efdbbecfdcda1a850f977868c46
Sebastian Song <sebsong@berkeley.edu>
2021-06-06 01:22:03 -0700

    fix other spots

--- keyboards/sofle/keymaps/helltm/keymap.c
+++ keyboards/sofle/keymaps/helltm/keymap.c
@@ -727,7 +727,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef ENCODER_ENABLE
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
             if(shift_held) {
@@ -757,6 +757,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             }
         }
     }
+
+    return true;
 }
 
 #endif
