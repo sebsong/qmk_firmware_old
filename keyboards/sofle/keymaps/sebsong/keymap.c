e80edf03c3e993b88071102b7c8b1cbfd3b0c98d
Sebastian Song <sebsong@berkeley.edu>
2022-06-27 21:30:04 -0700

    left, right scroll wheel

--- keyboards/sofle/keymaps/sebsong/keymap.c
+++ keyboards/sofle/keymaps/sebsong/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, KC_PTAB,   KC_UP, KC_NTAB, XXXXXXX,                          KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                          KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,        _______, XXXXXXX, KC_WH_L, KC_WH_R, XXXXXXX, XXXXXXX, _______,
                     _______, _______, _______, _______,   _______,     _______,  _______, _______, _______, _______
 )
 };
